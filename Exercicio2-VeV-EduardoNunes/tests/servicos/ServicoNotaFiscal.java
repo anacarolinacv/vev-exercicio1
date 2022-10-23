9b73338f85cc54d3b8882d1ca87387efe05fb6c8
Eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2022-10-23 12:32:29 -0300

    tests

--- Exercicio2-VeV-EduardoNunes/tests/servicos/ServicoNotaFiscal.java
+++ Exercicio2-VeV-EduardoNunes/tests/servicos/ServicoNotaFiscal.java
@@ -1,2 +1,46 @@
-package servicos;public class ServicoNotaFiscal {
+package servicos;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ServicoNotaFiscal {
+
+    ServicoNotaFiscal servicoNotaFiscal = new ServicoNotaFiscal();
+
+    private Fatura faturaConsultoria;
+
+    private Fatura faturaTreinamento;
+
+    private Fatura faturaOutroServico;
+
+
+    @BeforeEach
+    void setUp() {
+        TipoServico consultoria = CriaTipoServico.criar("CONSULTORIA");
+        TipoServico treinamento = CriaTipoServico.criar("TREINAMENTO");
+        TipoServico outra = CriaTipoServico.criar("Mentoria");
+
+        faturaConsultoria = new Fatura("Cliente", "Endereco", consultoria, 25.0);
+        faturaTreinamento = new Fatura("Cliente 1", "Endereço 1", treinamento, 20.0);
+        faturaOutroServico = new Fatura("Cliente 2", "Endereço 2", outra, 25.0);
+    }
+
+    @Test
+    void dadoTipoServicoConsultoriaOImpostoEhCalculado(){
+        NotaFiscal nf = servicoNotaFiscal.criaNotaFiscal(faturaConsultoria);
+        Assertions.assertEquals(nf.getValorImposto(), nf.getValor() * 0.35);
+    }
+
+    @Test
+    void dadoTipoServicoTreinamentoOImpostoEhCalculado(){
+        NotaFiscal nf = servicoNotaFiscal.criaNotaFiscal(faturaTreinamento);
+        Assertions.assertEquals(nf.getValorImposto(), nf.getValor() * 0.15);
+    }
+
+    @Test
+    void dadoOutroTipoServicoOImpostoEhCalculado(){
+        NotaFiscal nf = servicoNotaFiscal.criaNotaFiscal(faturaTreinamento);
+        Assertions.assertEquals(nf.getValorImposto(), nf.getValor().getValor() * 0.06);
+    }
 }
